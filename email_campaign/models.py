0e11b84deaf96dd3ebd4a8eae08febcdaf57e92c
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-09-08 18:39:49 +0530

    created Subscribers and Campaign models.

--- email_campaign/models.py
+++ email_campaign/models.py
@@ -1,3 +1,20 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+
+#Subscribers Model
+class Subscribers(models.Model):
+    first_name = models.CharField(max_length=25)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+
+
+#Campaign Model
+class Campaign(models.Model):
+    subject = models.CharField(max_length=100)
+    preview_text = models.TextField()
+    article_url = models.URLField()
+    html_content = models.TextField()
+    plain_text_content = models.TextField()
+    published_date = models.DateTimeField(default=datetime.now, blank=True)
